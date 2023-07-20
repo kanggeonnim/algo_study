0ebf9dacec5a7b25264c7636492c6967112fbccc
Seungmin Lee <mehot98@gmail.com>
2023-07-20 23:20:40 +0900

    16931

--- seungmin/implementation/BJO16931.java
+++ seungmin/implementation/BJO16931.java
@@ -1,12 +1,17 @@
 package implementation;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BJO16931 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
 		int[][] arr = new int[n][m];
 		
@@ -15,8 +20,9 @@ public class BJO16931 {
 		int under = n * m * 2;
 		
 		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < m; j++) {
-				arr[i][j] = sc.nextInt();
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
