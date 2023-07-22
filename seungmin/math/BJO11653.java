5cb18a335b6842d4e1bc6d1c25c40a505aaf14eb
Seungmin Lee <mehot98@gmail.com>
2023-07-22 23:03:35 +0900

    math

--- seungmin/math/BJO11653.java
+++ seungmin/math/BJO11653.java
@@ -6,14 +6,11 @@ public class BJO11653 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int i = 2;
 		
-		while (n > 1) {
-			if (n % i == 0) {
-				System.out.println(i); 
+		for(int i = 2; i <= n; i++) {
+			while(n % i == 0) {
 				n /= i;
-			} else {
-				i++;
+				System.out.println(i);
 			}
 		}
 	}
