a0cf376311693a49a5e7b2d3c1d8fa65dda4383d
mehot98 <76448240+mehot98@users.noreply.github.com>
2023-08-01 09:05:54 +0900

    [Data Structures] BJO1270

--- seungmin/data_structures/BJO1270.java
+++ seungmin/data_structures/BJO1270.java
@@ -1,11 +1,11 @@
-package data_structures;
+package day0731;
 
 import java.io.*;
 import java.util.*;
 
 public class BJO1270 {
 	public static void main(String[] args) throws IOException {
-		System.setIn(new FileInputStream("src/data_structures/1270.txt"));
+		System.setIn(new FileInputStream("src/day0731/1270.txt"));
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -16,7 +16,6 @@ public class BJO1270 {
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(in.readLine());
 			int ti = Integer.parseInt(st.nextToken());
-			int sum = 0;
 			map = new HashMap<Long, Integer>();
 			
 			for(int j = 0; j < ti; j++) {
@@ -24,13 +23,11 @@ public class BJO1270 {
 				
 				if (map.containsKey(temp)) map.put(temp, map.get(temp) + 1);
 				else map.put(temp, 1);
-				
-				sum++;
 			}
 			
 			Map.Entry<Long, Integer> temp = map.entrySet().stream().reduce((x, y) -> x.getValue() > y.getValue() ? x : y).get();
 			
-			System.out.println(temp.getValue() > sum / 2 ? temp.getKey() : "SYJKGW");
+			System.out.println(temp.getValue() > ti / 2 ? temp.getKey() : "SYJKGW");
 		}
 	}
 }
