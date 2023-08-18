9db95c8bae85345becb6232e7eeb0a5acc86f105
심지연 <sim.yyeon@gmail.com>
2023-08-18 16:31:17 +0900

    [통과-다시풀기]BOJ1992_쿼드트리.java

--- Simjiyeon/분할정복/BOJ1992_쿼드트리.java
+++ Simjiyeon/분할정복/BOJ1992_쿼드트리.java
@@ -1,6 +1,8 @@
-package week4;
+package DataStructure;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -11,6 +13,8 @@ public class BOJ1992_쿼드트리 {
 	static StringBuilder sb;
 
 	public static void main(String[] args) throws IOException {
+//		File fileName = new File("C:\\SSAFY\\workspace\\work-algo\\day0818\\src\\BOJ1992.txt");
+//		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 
@@ -26,62 +30,78 @@ public class BOJ1992_쿼드트리 {
 				input[i][j] = arr[j] - '0';
 			}
 		} // input
+		
+		if(N==1) {
+			System.out.println(input[0][0]);
+		}else {
+			div(0, 0, N / 2);
+			System.out.println(sb);
+		}
 
-		div(0, 0, N / 2);
-		System.out.println(sb);
 
 	}// main
 
 	static void div(int x, int y, int length) {
-		System.out.printf("x: %d, y: %d\n", x, y);
-		if (length == 1) {
-			sb.append(input[x][y]);
-			return;
-		}
-		// 4분면 검사
+		int si = x;
+		int sj = y;
+		int mi = x + length;
+		int mj = y + length;
+		int ei = x + length * 2;
+		int ej = y + length * 2;
+
 		sb.append("(");
-		// 제1
-		for (int i = x; i < x + length; i++) {
-			for (int j = y; i < y + length; j++) {
-				if (input[i][j] != input[x][y]) {
-					div(i, j, length / 2);
-				} else {
-					sb.append(input[x][y]);
-				}
-			}
+		int cnt = 0;
+		if (check(si, sj, mi, mj)) {
+			cnt++;
+			sb.append(input[si][sj]);
+		} else {
+			div(si, sj, length / 2);
+		}
+		// 왼위
+
+		if (check(si, mj, mi, ej)) {
+			cnt++;
+			sb.append(input[si][mj]);
+		} else {
+			div(si, mj, length / 2);
+		}
+		// 오위
+
+		if (check(mi, sj, ei, mj)) {
+			cnt++;
+			sb.append(input[mi][sj]);
+		} else {
+			div(mi, sj, length / 2);
+		}
+		// 왼아
+
+		if (check(mi, mj, ei, ej)) {
+			cnt++;
+			sb.append(input[mi][mj]);
+		} else {
+			div(mi, mj, length / 2);
+		}
+		sb.append(")");
+		if (cnt == 4&&input[si][sj]==input[si][mj]&&input[si][mj]==input[mi][sj]
+				&&input[mi][sj]==input[mi][mj]) {
+			sb.delete(sb.length() - 6, sb.length());
+			sb.append(input[si][sj]);
 		}
 
-		// 제2
-		for (int i = x; i < x + length; i++) {
-			for (int j = y + length; i < y + length * 2; j++) {
-				if (input[i][j] != input[x][y]) {
-					div(x, y + length, length / 2);
-				} else {
-					sb.append(input[x][y]);
-				}
-			}
-		}
-		// 제3
-		for (int i = x + length; i < x + length * 2; i++) {
-			for (int j = y; i < y + length; j++) {
-				if (input[i][j] != input[x][y]) {
-					div(x + length, y, length / 2);
-				} else {
-					sb.append(input[x][y]);
-				}
-			}
-		}
-
-		// 제4
-		for (int i = x + length; i <= x + length * 2; i++) {
-			for (int j = y + length; i <= y + length * 2; j++) {
-				if (input[i][j] != input[x][y]) {
-					div(x + length, y + length, length / 2);
-				} else {
-					sb.append(input[x][y]);
-				}
-			}
-		}
+		// 오아
 
 	}// div
+
+	static boolean check(int si, int sj, int ei, int ej) {
+		int x = input[si][sj];
+		for (int i = si; i < ei; i++) {
+			for (int j = sj; j < ej; j++) {
+				if (input[i][j] != x) {
+					return false;
+				}
+			}
+		}
+		return true;
+
+	}
 }
